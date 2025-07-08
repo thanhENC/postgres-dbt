766cef51d15456d2a682a66b1d1a351ff234fe62
thanhenc <thanhenc@gmail.com>
2025-07-08 21:46:18 +0700

    inject adw14 data

--- adw14/adw14data.sh
+++ adw14/adw14data.sh
@@ -1,3 +1,74 @@
+
+# MACOS
+# #!/bin/bash
+
+# echo "=============================================================="
+# echo "Downloading AdventureWorks OLTP database and installing it into PostgreSQL..."
+# echo "=============================================================="
+
+# # Download AdventureWorks-oltp-install-script.zip
+# curl -L -o AdventureWorks-oltp-install-script.zip https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks-oltp-install-script.zip
+
+# # Update package lists and install (or update) unzip, ruby, and postgresql-client-common
+# echo "=============================================================="
+# echo "Updating package lists and installing (or updating) unzip, ruby, and postgresql-client..."
+# echo "=============================================================="
+# sudo apt update
+# sudo apt install -y unzip
+# sudo apt install -y ruby
+# sudo apt-get install -y postgresql-client
+
+# # Unzip the downloaded file into the /data directory
+# echo "=============================================================="
+# echo "Unzipping the downloaded file into the /data directory..."
+# echo "=============================================================="
+# unzip AdventureWorks-oltp-install-script.zip -d ./data
+
+# # Optional: Check if the unzip process was successful
+# if [ -d "/data" ]; then
+#     echo "Download and unzip completed successfully into /data."
+# else
+#     echo "Something went wrong. Please check the script."
+# fi
+
+# # Download install adventureworks database script
+# echo "=============================================================="
+# echo "Downloading install.sql script..."
+# echo "=============================================================="
+# curl -L -o install.sql https://raw.githubusercontent.com/lorint/AdventureWorks-for-Postgres/master/install.sql
+# cp install.sql ./data/install.sql
+
+# # Download update csv files script
+# echo "=============================================================="
+# echo "Downloading update_csvs.rb script..."
+# echo "=============================================================="
+# curl -L -o update_csvs.rb https://raw.githubusercontent.com/lorint/AdventureWorks-for-Postgres/master/update_csvs.rb
+# cp update_csvs.rb ./data/update_csvs.rb
+
+# # connect to OLTP postgres database
+# echo "=============================================================="
+# echo "Connecting to OLTP postgres database..."
+# echo "=============================================================="
+# set -a
+# source ../docker/.env
+# set +a
+
+# # Modify the CSV files to make them work with PostgreSQL
+# cd ./data
+# ruby update_csvs.rb
+
+# # Create the OLTP database and tables
+# export OLTP_DB_CONNECTION_STRING=postgresql://$OLTP_DB_USER:$OLTP_DB_PASSWORD@localhost:$OLTP_DB_PORT_EXPOSE/$OLTP_DB_NAME
+# psql $OLTP_DB_CONNECTION_STRING -f install.sql
+
+# # Optional: Check if the database was created successfully
+# if [ $? -eq 0 ]; then
+#     echo "Database created successfully."
+# else
+#     echo "Something went wrong. Please check the script."
+# fi
+
+# LINUX
 #!/bin/bash
 
 echo "=============================================================="
@@ -5,16 +76,7 @@ echo "Downloading AdventureWorks OLTP database and installing it into PostgreSQL
 echo "=============================================================="
 
 # Download AdventureWorks-oltp-install-script.zip
-curl -L -o AdventureWorks-oltp-install-script.zip https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks-oltp-install-script.zip
-
-# Update package lists and install (or update) unzip, ruby, and postgresql-client-common
-echo "=============================================================="
-echo "Updating package lists and installing (or updating) unzip, ruby, and postgresql-client..."
-echo "=============================================================="
-sudo apt update
-sudo apt install -y unzip
-sudo apt install -y ruby
-sudo apt-get install -y postgresql-client
+wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks-oltp-install-script.zip
 
 # Unzip the downloaded file into the /data directory
 echo "=============================================================="
@@ -33,30 +95,28 @@ fi
 echo "=============================================================="
 echo "Downloading install.sql script..."
 echo "=============================================================="
-curl -L -o install.sql https://raw.githubusercontent.com/lorint/AdventureWorks-for-Postgres/master/install.sql
-cp install.sql ./data/install.sql
+wget https://raw.githubusercontent.com/lorint/AdventureWorks-for-Postgres/master/install.sql -P ./data
 
 # Download update csv files script
 echo "=============================================================="
 echo "Downloading update_csvs.rb script..."
 echo "=============================================================="
-curl -L -o update_csvs.rb https://raw.githubusercontent.com/lorint/AdventureWorks-for-Postgres/master/update_csvs.rb
-cp update_csvs.rb ./data/update_csvs.rb
+wget https://raw.githubusercontent.com/lorint/AdventureWorks-for-Postgres/master/update_csvs.rb -P ./data
 
 # connect to OLTP postgres database
 echo "=============================================================="
 echo "Connecting to OLTP postgres database..."
 echo "=============================================================="
-set -a
-source ../docker/.env
-set +a
+# set -a
+# source ../docker/.env
+# set +a
 
 # Modify the CSV files to make them work with PostgreSQL
 cd ./data
 ruby update_csvs.rb
 
 # Create the OLTP database and tables
-export OLTP_DB_CONNECTION_STRING=postgresql://$OLTP_DB_USER:$OLTP_DB_PASSWORD@localhost:$OLTP_DB_PORT_EXPOSE/$OLTP_DB_NAME
+export OLTP_DB_CONNECTION_STRING=postgresql://$OLTP_DB_USER:$OLTP_DB_PASSWORD@$OLTP_DB_HOST:5432/$OLTP_DB_NAME
 psql $OLTP_DB_CONNECTION_STRING -f install.sql
 
 # Optional: Check if the database was created successfully
